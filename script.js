3ad927971cae13be822160147d1fb515ca33931e
royal4442 <103133596+royal4442@users.noreply.github.com>
2026-07-26 01:30:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+document.querySelectorAll("nav a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        nav.classList.remove("active");
+
+    });
+
+});
+
 // ===============================
 // Smooth Scrolling
 // ===============================
